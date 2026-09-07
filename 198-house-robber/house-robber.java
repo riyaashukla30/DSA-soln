b2a8a86f7eb9762be26c007debdf288b377d3b34
Riya Shukla <riyaashukla30@gmail.com>
2026-09-07 21:17:46 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (19.29%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,13 +2,11 @@ class Solution {
     public int rob(int[] nums) {
         int prev2=0;
         int prev1=0;
-
-        for (int num: nums){
-            int curr=Math.max(prev1, prev2 + num);
-            prev2 = prev1;
-            prev1= curr;
+        for(int money:nums){
+            int current=Math.max(prev1, prev2 + money);
+            prev2=prev1;
+            prev1 = current;
         }
-
         return prev1;
         
     }
